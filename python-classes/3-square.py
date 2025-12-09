dead4971cfc80a9f794380cd3eb66abebcdad377
kamil <nezerovk425@gmail.com>
2025-12-09 10:11:02 +0000

    To get value of privat attribute via setter and getter method

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -12,6 +12,11 @@ class Square:
     def __init__(self, size=0):
         self.__size = size
     
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter    
     def size(self, value):
         if isinstance(value, int):
             self.__size = value
@@ -19,9 +24,6 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-    
-    def size(self):
-        return self.__size
     
     def area(self):
         return self.__size ** 2
