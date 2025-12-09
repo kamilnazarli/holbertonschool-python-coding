29f19164f6fadf456754e2045e6a534bc3fc8a72
kamil <nezerovk425@gmail.com>
2025-12-09 09:30:14 +0000

    To make size private

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 class Square:
+    """
+    A class to represent a square.
+    
+    Attributes:
+        __size(int): The size of the square's sides.
+    """
     def __int__(self, size):
+        """
+        Initialize a new Square instance.
+        
+        Parameters:
+        size (int): The size of the square's sides.
+        """
         self.__size = size
